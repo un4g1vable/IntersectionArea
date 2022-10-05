2615a10f581ec012c9535d14fcedfc7fdd573823
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-06 00:56:37 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  double a1x, a1y, a2x, a2y;
+  double b1x, b1y, b2x, b2y;
+
+  cout << "Первый квадрат: ";
+  cin >> a1x >> a1y >> a2x >> a2y;
+
+  cout << "Второй квадрат: ";
+  cin >> b1x >> b1y >> b2x >> b2y;
+
+  double peresechenieX = min(a2x, b2x) - max(a1x, b1x);
+  double peresechenieY = min(a2y, b2y) - max(a1y, b1y);
+
+  if (peresechenieX <= 0 or peresechenieY <= 0)
+    cout << "Нет пересечения";
+  else
+  {
+    double S = peresechenieX * peresechenieY;
+
+    cout << "Площадь пересечения: " << S << endl;
+  }
+}
