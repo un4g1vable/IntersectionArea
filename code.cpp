c67ef7e7139f296875c3b570a554257ba93b5b63
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-06 14:52:03 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -8,21 +8,21 @@ int main()
   double a1x, a1y, a2x, a2y;
   double b1x, b1y, b2x, b2y;
 
-  cout << "Первый квадрат: ";
+  cout << "Первый прямоугольник: ";
   cin >> a1x >> a1y >> a2x >> a2y;
 
-  cout << "Второй квадрат: ";
+  cout << "Второй прямоугольник: ";
   cin >> b1x >> b1y >> b2x >> b2y;
 
   double peresechenieX = min(a2x, b2x) - max(a1x, b1x);
   double peresechenieY = min(a2y, b2y) - max(a1y, b1y);
 
   if (peresechenieX <= 0 or peresechenieY <= 0)
-    cout << "Нет пересечения";
+    cout << "нет пересечения";
   else
   {
     double S = peresechenieX * peresechenieY;
 
-    cout << "Площадь пересечения: " << S << endl;
+    cout << "площадь пересечения: " << S << endl;
   }
 }
